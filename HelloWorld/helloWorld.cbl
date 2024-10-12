5a39a3541a9cb617074fbf4945f2166364284156
Dreamer899 <je626883@gmail.com>
2024-10-12 16:10:51 -0300

    Create helloWorld.cbl

---
+++ HelloWorld/helloWorld.cbl
@@ -0,0 +1,26 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD-COBOL.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       
+       INPUT-OUTPUT SECTION.
+       
+       DATA DIVISION.
+       
+       FILE SECTION.
+       
+       WORKING-STORAGE SECTION.
+       
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "HELLO WORLD".
+           
+           
+           
+           
+           STOP RUN.
+           
+           END PROGRAM HELLO-WORLD-COBOL.
+           
+      
+     
